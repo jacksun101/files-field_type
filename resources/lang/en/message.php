26ca5bb25bef8fadbf79e80d275af710c188574b
Ryan Thompson <service@aiwebsystems.com>
2015-11-29 02:34:23 -0600

    Don't reload the table over and over. No sense in it.

--- resources/lang/en/message.php
+++ resources/lang/en/message.php
@@ -4,4 +4,6 @@ return [
     'choose_files'      => 'Which files would you like to use?',
     'choose_folder'     => 'Which folder would you like to upload to?',
     'no_files_selected' => 'No files selected.',
+    'uploading'         => 'Uploading',
+    'loading'           => 'Loading'
 ];
