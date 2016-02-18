d5d46a130399016ec790f40f68aa4bf5491e463e
Ryan Thompson <ryan@pyrocms.com>
2016-02-18 14:55:23 -0600

    Changes

--- resources/js/input.js
+++ resources/js/input.js
@@ -1,10 +1,11 @@
 $(function () {
 
     // Initialize file pickers
-    $('.files-field_type').each(function () {
+    $('[data-provides="anomaly.field_type.files"]').each(function () {
 
-        var wrapper = $(this);
-        var field = wrapper.data('field');
+        var input = $(this);
+        var field = input.data('field_name');
+        var wrapper = input.closest('.form-group');
         var modal = $('#' + field + '-modal');
 
         var selected = $('[name="' + field + '"]').val().split(',');
